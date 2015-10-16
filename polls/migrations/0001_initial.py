caa84e1401bbff8ae332d1cf63ddce1703416283
Igor Pavlov <i.pavlov@socialquantum.com>
2015-10-16 18:00:10 +0300

    created first migration

---
+++ polls/migrations/0001_initial.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Applicator',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(default=b'', max_length=200)),
+                ('info', models.TextField()),
+                ('avatar_url', models.FilePathField()),
+            ],
+        ),
+        migrations.CreateModel(
+            name='ApplicatorSkill',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('skill_level', models.IntegerField(default=0)),
+                ('applicator_id', models.ForeignKey(to='polls.Applicator')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Company',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=200)),
+                ('info', models.TextField()),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Skill',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=200)),
+                ('info', models.TextField()),
+                ('applicators', models.ManyToManyField(to='polls.Applicator', through='polls.ApplicatorSkill')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='User',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('email', models.EmailField(max_length=254)),
+                ('password', models.CharField(default=b'', max_length=200)),
+                ('created_at', models.DateTimeField()),
+                ('updated_at', models.DateTimeField(auto_now=True)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Vacancy',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=200)),
+                ('info', models.TextField()),
+                ('region', models.IntegerField()),
+                ('company', models.ForeignKey(to='polls.Company')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='VacancySkill',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('necessary_level', models.IntegerField(default=0)),
+                ('skill', models.ForeignKey(to='polls.Skill')),
+                ('vacancy', models.ForeignKey(to='polls.Vacancy')),
+            ],
+        ),
+        migrations.AddField(
+            model_name='vacancy',
+            name='skills',
+            field=models.ManyToManyField(to='polls.Skill', through='polls.VacancySkill'),
+        ),
+        migrations.AddField(
+            model_name='company',
+            name='user',
+            field=models.ForeignKey(to='polls.User'),
+        ),
+        migrations.AddField(
+            model_name='applicatorskill',
+            name='skill',
+            field=models.ForeignKey(to='polls.Skill'),
+        ),
+        migrations.AddField(
+            model_name='applicator',
+            name='user',
+            field=models.ForeignKey(to='polls.User'),
+        ),
+    ]
--- polls/models.py
+++ polls/models.py
@@ -1,14 +1,13 @@
 from django.db import models
 
-# Create your models here.
 
 class User(models.Model):
 	email = models.EmailField()
 	password = models.CharField(max_length=200,default="")
 	created_at = models.DateTimeField()
-	updated_at = models.DateTimeField(auto_now=True,auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
 
-class Application(models.Model):
+class Applicator(models.Model):
 	name = models.CharField(max_length=200,default="")
 	info = models.TextField()
 	avatar_url = models.FilePathField()
@@ -17,10 +16,10 @@ class Application(models.Model):
 class Skill(models.Model):
 	name = models.CharField(max_length=200)
 	info = models.TextField()
-	applications = models.ManyToMany(Application, through='ApplicationSkill')
+	applicators = models.ManyToManyField(Applicator, through='ApplicatorSkill')
 
-class ApplicationSkill(models.Model):
-	application_id = models.ForeignKey(Application)
+class ApplicatorSkill(models.Model):
+	applicator_id = models.ForeignKey(Applicator)
 	skill = models.ForeignKey(Skill)
 	skill_level = models.IntegerField(default = 0)
 
@@ -33,10 +32,10 @@ class Vacancy(models.Model):
 	title = models.CharField(max_length=200)
 	info = models.TextField()
 	company = models.ForeignKey(Company)
-	region = models.IntegerKey()
+	region = models.IntegerField()
 	skills = models.ManyToManyField(Skill, through='VacancySkill')
 
 class VacancySkill(models.Model):
-	skill = models.Foreignkey(Skill)
+	skill = models.ForeignKey(Skill)
 	vacancy = models.ForeignKey(Vacancy)
 	necessary_level = models.IntegerField(default = 0) 
