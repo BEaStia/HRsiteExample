6195ad578fffc5f6e06388dc5a06e383e83cc684
Igor Pavlov <i.pavlov@socialquantum.com>
2015-10-16 18:46:35 +0300

    added test for creating user

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
-
+import datetime
 # Create your tests here.
+
+
+class CreateUserTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_user_creation(self):
+        u = User(email="gophan1992@gmail.com", password='111')
+        u.created_at = datetime.datetime.now()
+        u.save()
+        self.assertEqual(u.email, 'gophan1992@gmail.com')
